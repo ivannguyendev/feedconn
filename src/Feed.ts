16975dc800903c723307ec838ecab926baab757a
Ivan Nguyen <ivannguyen@Ivan-Mac-4.local>
2022-07-18 15:29:41 +0700

    U: count feed

--- src/Feed.ts
+++ src/Feed.ts
@@ -3,21 +3,31 @@ import { Reference } from '@firebase/database-types';
 import { Message, FeedBuilder } from '@whthduck/feed-builder';
 import { Base } from './Base';
 import { LogFeed } from './LogFeed';
+import { TagFeed } from './TagFeed';
 
 export class Feed extends Base {
   feedRef: Reference;
   logFeed: LogFeed;
+  tagFeed: TagFeed;
 
   constructor(app?: admin.app.App) {
     super(app);
     this.feedRef = this.database.ref('feeds');
     this.logFeed = new LogFeed(app);
+    this.tagFeed = new TagFeed(app);
+  }
+
+  async countFeed(path: string) {
+    const snapshot = await this.feedRef.child(path).child('count').get();
+    return snapshot.val() || 0;
   }
 
   async add(userId: string, message: Partial<Message>) {
     const feedMessage = FeedBuilder.render({ ...message, userId });
     const feedId = feedMessage._id;
     const path = `${userId}/${feedId}`;
+    const countFeed = await this.countFeed(path);
+    feedMessage.count += countFeed;
     await this.feedRef.child(path).set(feedMessage);
     await this.logFeed.add(this.feedRef.key, feedId, path);
     return feedMessage;
@@ -31,8 +41,10 @@ export class Feed extends Base {
 
   async setReadState(feedId: string) {
     const logData = await this.logFeed.find(this.feedRef.key, feedId);
-    await this.feedRef.child(logData.path).child('state').set('read');
-    await this.logFeed.remove(feedId, 'unread');
+    await this.feedRef.child(logData.path).update({
+      state: 'read',
+    });
+    await this.tagFeed.remove(feedId, 'unread');
     return true;
   }
 }
